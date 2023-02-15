9c2951f3825ef6dc5d80866a71b4b0632c76a430
bsanket16 <bhukansanket16@gmail.com>
2023-02-15 12:25:21 +0530

    checking animartion

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,6 +7,8 @@ import axios from 'axios';
 
 import fl from '../../assets/xrcnew.png'
 
+import AnimatedText from 'react-animated-text-content'
+
 const Footer = () => {
 
 
@@ -23,7 +25,7 @@ const Footer = () => {
 
         <div className='footer-options-container-one'>
 
-          {/* <AnimatedTexts
+          <AnimatedText
             type="words" // animate words or chars
             animation={{
               x: '200px',
@@ -41,7 +43,7 @@ const Footer = () => {
             rootMargin="20%"
           >
           WEAR CLOTHES THAT DON'T EXIST.
-          </AnimatedTexts> */}
+          </AnimatedText>
           
 
 
@@ -55,13 +57,13 @@ const Footer = () => {
           </div> */}
 
 
-          <div className='footer-options-title pt-5'>
+          {/* <div className='footer-options-title pt-5'>
           WEAR
           CLOTHES
           THAT
           DON'T
           EXIST.
-          </div>
+          </div> */}
 
           <div className='footer-subscribe-form'>
 
