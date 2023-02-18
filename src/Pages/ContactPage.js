993efbc6e6ed592ad9d8652a7e79919c8e454037
bsanket16 <bhukansanket16@gmail.com>
2023-02-18 16:20:09 +0530

    seo changes

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -4,9 +4,18 @@ import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
 import ScrollToTop from './ScrollToTop'
 
+import {Helmet} from 'react-helmet'
+
 function ContactPage() {
   return (
     <div>
+      <Helmet>
+        <title>Contact | XR Couture</title>
+        <meta
+          name="description"
+          content="Want to get in touch? We'd love to hear from you. Write to us at hello@xrcouture.com"
+        />
+      </Helmet>
       <ScrollToTop />
       <Header />
       <Contact />
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Brand from '../Components/brands/Brand'
 import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
@@ -10,6 +11,13 @@ import ScrollToTop from './ScrollToTop'
 function Homepage() {
   return (
     <div>
+      <Helmet>
+        <title>XR Couture | Digital Fashion | Metaverse | AR/VR | NFTs</title>
+        <meta
+          name="description"
+          content="XR Couture is a leading digital wearables platform that creates hyper-realistic 3D digital wearables which can be worn on avatars across multiple metaverse platforms and online games."
+        />
+      </Helmet>
       <ScrollToTop />
       <Header />
       <Section1 />
